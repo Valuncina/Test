0c8247c4769acb39fb65acb867089865f00f32a2
Valuncina <litinghao54188@163.com>
2026-09-16 22:41:35 +0800

    Version1

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//a sample of the easiest c++ code
+
+#include <iostream>  //library header
+
+int main()           //the main func
+{
+    std::cout << "hello world" << std::endl ;   //say hello
+
+    return 0;        //end with 0
+}
